7a3cc01f9ebb74646357dd1b822a2316bf445dcf
Manu Farfaro <manu.farfaro@gmail.com>
2025-07-09 22:25:04 -0300

    chore: go back main module to the original plain hello rust string.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,3 @@
-#[derive(Debug)]
-struct Deck {
-    cards: Vec<String>,
-}
-
 fn main() {
-    let deck = Deck { cards: vec![] };
-    println!("Here's your deck: {:?}", deck);
+    println!("Hello Rust!");
 }
