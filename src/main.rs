2129136e9abfc8253b8e258dabec484e42fc373c
Manu Farfaro <manu.farfaro@gmail.com>
2025-07-12 18:21:03 -0300

    feat: initial deck struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use rand::{rng, seq::SliceRandom};
+
+#[derive(Debug)]
+struct Deck {
+    cards: Vec<String>,
+}
+
+impl Deck {
+    fn new() -> Self {
+        let suits = ["Hearts", "Spades", "Diamonds"];
+        let values = ["Ace", "Two", "Three"];
+
+        let mut cards = vec![];
+
+        for suit in suits {
+            for value in values {
+                let card = format!("{} of {}", value, suit);
+                cards.push(card);
+            }
+        }
+
+        Deck { cards }
+    }
+    fn shuffle(&mut self) {
+        let mut rng = rng();
+        self.cards.shuffle(&mut rng);
+    }
+}
+
 fn main() {
-    println!("Hello Rust!");
+    let mut deck = Deck::new();
+    deck.shuffle();
+
+    println!("Heres your deck: {:#?}", deck);
 }
