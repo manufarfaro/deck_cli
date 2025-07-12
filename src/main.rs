1ae75ee1e00287ad5d34ef2e5a4eacad499b4ba2
Manu Farfaro <manu.farfaro@gmail.com>
2025-07-12 15:12:16 -0300

    Revert "feature/update-readme"
    This reverts commit b2d8e9e7708dddd9a75b8dda6b6edcf93d145bc7.

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,3 @@
-#[derive(Debug)]
-struct Deck {
-    cards: Vec<String>,
-}
-
-impl Deck {
-    fn new() -> Self {
-        let suits = ["Hearts", "Spades", "Diamonds"];
-        let values = ["Ace", "Two", "Three"];
-
-        let mut cards = vec![];
-
-        for suit in suits {
-            for value in values {
-                let card = format!("{} of {}", value, suit);
-                cards.push(card);
-            }
-        }
-
-        let deck = Deck { cards };
-        return deck;
-    }
-}
-
 fn main() {
-    let deck = Deck::new();
-
-    println!("Heres your deck: {:#?}", deck);
+    println!("Hello Rust!");
 }
