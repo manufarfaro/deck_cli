44ec226bea86ca9523b499eff9bcf55b48e4d31c
Manu Farfaro <manu.farfaro@gmail.com>
2025-07-12 18:43:21 -0300

    feat: initial deck struct (#9)
    * feat: initial deck struct
    
    * fix: lint issue around print the deck
    
    * fix: lint issue around print the card on terminal.
    
    * feat: add deal function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use rand::{rng, seq::SliceRandom};
+
+#[derive(Debug)]
+struct Deck {
+    cards: Vec<String>,
+}
+
+impl Deck {
+    fn new() -> Self {
+        let suits = ["Hearts", "Spades", "Diamonds"];
+        let values = ["Ace", "Two", "Three"];
+
+        let mut cards = vec![];
+
+        for suit in suits {
+            for value in values {
+                let card = format!("{value} of {suit}");
+                cards.push(card);
+            }
+        }
+
+        Deck { cards }
+    }
+    fn shuffle(&mut self) {
+        let mut rng = rng();
+        self.cards.shuffle(&mut rng);
+    }
+
+    fn deal(&mut self, num_cards: usize) -> Vec<String> {
+        self.cards.split_off(self.cards.len() - num_cards)
+    }
+}
+
 fn main() {
-    println!("Hello Rust!");
+    let mut deck = Deck::new();
+    deck.shuffle();
+
+    let cards = deck.deal(3); // TODO: add error handling
+
+    println!("Heres your hand: {cards:#?}");
+    println!("Heres your deck: {deck:#?}");
 }
